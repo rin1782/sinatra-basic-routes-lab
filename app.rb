4df8012d9b74452cda7ac72b26f0a5b650b41528
Erin Cola <colaerin@yahoo.com>
2021-01-08 12:55:01 -0500

    commit tree 50e27f242e17e0007d6a5e4db70ab023d7c7c0e3

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+
+    get '/name' do
+        "My name is Erin"
+    end
+
+    get '/hometown' do 
+        "My hometown is Binghamton"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is I wanna dance with somebody"
+    end
+    end
